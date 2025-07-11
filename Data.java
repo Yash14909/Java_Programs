8c94310612c45d95eeb9d8266c62b3ef84e1d399
Yash14909 <yashvreddy2043@gmail.com>
2025-07-11 23:45:59 +0530

    deca commit

---
+++ Data.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Model
+{
+String Name;
+String Email;
+Long phone;
+public Model (String Name,String Email,Long phone)
+{
+   this.Name=Name;
+   this.Email=Email;
+   this.phone=phone;
+}
+@Override
+public String toString()
+{
+    return this.Name + "," + this.Email + "," + this.phone;
+    }
+}
+public class Data
+{
+   public static void main (String [] args)
+     {
+        List<Model> list = new ArrayList<>();
+        System.out.println("Before adding the data to the list ");
+        System.out.println("Size of the list=" +list.size());
+        System.out.println("List data=" + list);
+        String Name = "GitHub";
+        String Email = "github_39@gmail.com";
+        Long phone = Long.valueOf (String.valueOf (1234567890));
+        list.add(new Model(Name,Email,phone));
+        System.out.println("\nAfter adding data to the list.....");
+        System.out.println("Size of list=" + list.size());
+        System.out.println("List data=" + list.get(0).toString());
+  }
+}
+
+
+
---
+++ README.md
@@ -0,0 +1,25 @@
+# Java Model List Demo
+
+This is a simple Java program that demonstrates the use of:
+
+- A custom `Model` class with attributes `Name`, `Email`, and `Phone`
+- Java `ArrayList` to store instances of the `Model`
+- Overriding `toString()` for clean object printing
+
+
+### 🚀 How to Run
+
+1. Compile the Java file:
+   ```bash
+   javac Data.java
+   java Data
+
+📝 Output
+Before adding the data to the list 
+Size of the list=0
+List data=[]
+After adding data to the list.....
+Size of list1
+List data=GitHub,github_39@gmail.com,1234567890
+
+
